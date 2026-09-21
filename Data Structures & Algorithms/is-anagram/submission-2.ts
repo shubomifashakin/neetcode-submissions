a8045b6ef9c16192b1f065cde9d6b35aa36157c0
Olashubomi Fashakin <subomifasakin@outlook.com>
2026-09-21 12:45:18 +0100

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.ts
@@ -0,0 +1,49 @@
+// // this uses sorting and then we compare each character with each other
+// class Solution {
+//     /**
+//      * @param {string} s
+//      * @param {string} t
+//      * @return {boolean}
+//      */
+//     isAnagram(s: string, t: string): boolean {
+//         // if they are not the same length then they are not anagrams, false
+//         if(s.length !== t.length)return false
+
+//         // split the characters of stringA, so it becomes an array, then sort it
+//         const splitA= s.split('').sort()
+
+//         // split the characters of stringB, so it becomes an array then sort it
+//         const splitB= t.split("").sort()
+      
+//         let checked=0
+//         // so far we havent checked everything
+//       while(checked!=splitB.length){
+//         if(splitA[checked]!==splitB[checked]){
+//             return false
+//         }
+//         checked=checked+1
+//       }
+
+//         return true
+//     }
+// }
+
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        // if they are not the same length then they are not anagrams, false
+        if(s.length !== t.length)return false
+
+        // split the characters of stringA, so it becomes an array, then sort it
+        const splitA= s.split('').sort().join("")
+
+        // split the characters of stringB, so it becomes an array then sort it
+        const splitB= t.split("").sort().join("")
+      
+        return splitB===splitA
+    }
+}
